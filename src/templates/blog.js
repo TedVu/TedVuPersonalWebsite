b82a71faea434940adce58b8bbff1765a02b40b8
TedVu <tedvu184@gmail.com>
2020-06-23 15:19:22 +1000

    Fix typo

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 
