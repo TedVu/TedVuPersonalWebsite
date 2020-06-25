177fb131c3bafa13aa80a3f286aa6bbe8d5674e2
TedVu <tedvu184@gmail.com>
2020-06-25 11:23:10 +1000

    Change image link

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,7 @@ const aboutPage = () => {
                 <Head title="About" />
                 <h1 className={aboutStyles.title}>About Me</h1>
                 <div className={aboutStyles.aboutMe}>
-                    <img className={aboutStyles.img} src="grayLinkedInAva2020.jpg" alt="tedvu" width="200" height="250" />
+                    <img className={aboutStyles.img} src="./grayLinkedInAva2020.jpg" alt="tedvu" width="200" height="250" />
                   <div className={aboutStyles.content}> Hi there, my name is Ted, I'm a third year Software Engineering student at RMIT Melbourne. This is my <a href="https://www.dropbox.com/s/nyt0hmi0cajen2n/Ted_CV_2020.pdf?dl=0" target="_blank">journey </a>so far.
                   <br/><br/>
                   I really like exploring Software Engineering area with the focus on: Building large scale software and Algorithms. 
