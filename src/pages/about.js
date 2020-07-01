4cf2b985b1317d7bc66b0db4829fffd3d12665af
TedVu <tedvu184@gmail.com>
2020-07-01 20:51:51 +1000

    Add link style

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,11 +17,11 @@ const aboutPage = () => {
                 <h1 className={aboutStyles.title}>About Me</h1>
                 <div className={aboutStyles.aboutMe}>
                     <img className={aboutStyles.img} src="./grayLinkedInAva2020.jpg" alt="tedvu" height="300" width="250"/>
-                  <div className={aboutStyles.content}> Hi there, my name is Ted, I'm a third year Software Engineering student at RMIT Melbourne. Here is my <a href="https://www.dropbox.com/s/nyt0hmi0cajen2n/Ted_CV_2020.pdf?dl=0" target="_blank">journey </a>so far and I enjoy every minute of it.
+                  <div className={aboutStyles.content}> Hi there, my name is Ted, I'm a third year Software Engineering student at RMIT Melbourne. Here is my <a href="https://www.dropbox.com/s/nyt0hmi0cajen2n/Ted_CV_2020.pdf?dl=0" target="_blank" className={aboutStyles.links}>journey </a>so far and I enjoy every minute of it.
                   <br/><br/>
                   I really like exploring Software Engineering area with the focus on: Building Large Scale Software System and Algorithms. 
                   <br/><br/>
-                  My spirit animal is the <a href="https://www.youtube.com/watch?v=80a7f6iKet4" target="_blank">Sloth</a> (I mean who doesn't like the Sloth) and my guilty pleasure is watching HIMYM marathon rerun :)
+                  My spirit animal is the <a href="https://www.youtube.com/watch?v=80a7f6iKet4" target="_blank" className={aboutStyles.links}>Sloth</a> (I mean who doesn't like the Sloth) and my guilty pleasure is watching HIMYM marathon rerun :)
                   </div>
                 </div>
             </Layout>
