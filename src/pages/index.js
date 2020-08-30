b28d4306d996950aa8feb796d20e4ce4786ec96e
Ted Vu <tedvu184@gmail.com>
2020-08-30 13:33:12 +1000

    Add quote

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,28 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from "react"
+import Layout from "../components/layout"
 
-import Head from '../components/head'
-import indexStyle from './index.module.scss'
+import Head from "../components/head"
+import indexStyle from "./index.module.scss"
 const indexPage = () => {
-
   return (
     <Layout>
       <Head title="Home" />
       <div className={indexStyle.introduction}>
-       <h2>Welcome to Ted Vu Personal Website</h2>
-       <div>This is where I write my personal thoughts about engineering, tech, life.</div>
-       <div><br/>Have a look around !!!</div>
+        <h2>Welcome to Ted Vu Personal Website</h2>
+        <div>
+          This is where I write my personal thoughts about engineering, tech,
+          life.
+        </div>
+        <blockquote>
+          Scientists discover the world that exists Engineers create the world
+          that never was.
+        </blockquote>
+        <div className={indexStyle.cite}>
+          <cite>Theodore Von Karman</cite>
+        </div>
       </div>
     </Layout>
-
   )
-
 }
-
 
 export default indexPage
