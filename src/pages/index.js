c19f90e864defb937209fe932acd85fa81997b5c
Ted Vu <tedvu184@gmail.com>
2020-10-05 21:49:55 +1100

    Change background and add close effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,23 @@ import Layout from "../components/layout"
 import Head from "../components/head"
 import indexStyle from "./index.module.scss"
 import LightSpeed from "react-reveal/LightSpeed"
-// import HeadRoom from "react-headroom"
+
 const indexPage = () => {
+  function handleClick(e) {
+    e.preventDefault()
+    document.getElementById("welcome-panel").style.display = "none"
+  }
   return (
     <Layout>
       <Head title="Home" />
       <LightSpeed left>
-        <div className={indexStyle.introduction}>
-          <h2>Welcome to Ted Vu Personal Website</h2>
+        <div id="welcome-panel" className={indexStyle.introduction}>
+          <h2>
+            Welcome to Ted Vu Personal Website
+            <button className={indexStyle.closeButton} onClick={handleClick}>
+              CLOSE
+            </button>
+          </h2>
           <div>
             This is where I write my personal thoughts about engineering, tech,
             life and so much more
---
+++ src/pages/scrolleffect.js
@@ -0,0 +1,13 @@
+window.onscroll = function () {
+  handleScroll()
+}
+
+function handleScroll() {
+  //   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  //     document.getElementById("welcome-panel").style.display = "none"
+  //   } else {
+  //     document.getElementById("welcome-panel").style.display = ""
+  //   }
+
+  console.log("DEBUG")
+}
