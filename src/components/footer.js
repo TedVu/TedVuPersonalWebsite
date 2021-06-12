f4b92e8b58bd51bbcad0e265c11aefec5a87ac28
Ted Vu <tedvu184@gmail.com>
2021-06-13 01:24:16 +1000

    Change footer Make gatsbyjs link visible Update year

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import {graphql, useStaticQuery} from 'gatsby'
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
-import footerStyles from './footer.module.scss'
+import footerStyles from "./footer.module.scss"
 
 const Footer = () => {
-
-    const data = useStaticQuery(graphql`
-    query{
-        site{
-          siteMetadata{
-            author
-          }
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
         }
       }
-    `)
-    return (
-        <footer className={footerStyles.footer}>
-            <p>Created by {data.site.siteMetadata.author}, copyright 2020, proudly powered by GatsbyJS</p>
-        </footer>
-    )
+    }
+  `)
+  return (
+    <footer className={footerStyles.footer}>
+      <p>
+        Created by {data.site.siteMetadata.author}, copyright 2021, proudly
+        powered by{" "}
+        <a
+          href="https://gatsbyjs.com"
+          target="_blank"
+          style={{ color: "yellow" }}
+        >
+          GatsbyJS{" "}
+        </a>
+      </p>
+    </footer>
+  )
 }
 
 export default Footer
