675280c39c2c6cedf2a42c5f27d9153ea2cfed64
TedEngineer <tedvu184@gmail.com>
2021-12-11 10:41:29 +1100

    Update date updated top 100

--- src/pages/top100.js
+++ src/pages/top100.js
@@ -13,7 +13,7 @@ const Top100Page = () => {
           <div className={top100Styles.content}>
             <h3>
               Here is the list of top 100 things I want to do in my life. Last
-              updated: 25-08-2021.{" "}
+              updated: 5-12-2021.{" "}
             </h3>
             <h4>
               Have any suggestion ? Just send me an{" "}
