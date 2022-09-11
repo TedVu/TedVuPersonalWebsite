192ae38e03d025d788ae7838df45aa08662a3c1a
TedEngineer <tedvu184@gmail.com>
2022-09-11 21:46:57 +1000

    Revert to 475fae8

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,7 +4,6 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 import blogStyles from "./blog.module.scss"
 import Head from "../components/head"
 import Bounce from "react-reveal/Bounce"
-import { format } from "prettier"
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -20,17 +19,6 @@ const BlogPage = () => {
       }
     }
   `)
-
-  const uniqueBlogPosts = []
-  const blogPostTitle = new Set()
-  data.allContentfulBlogPost.edges.forEach(edge => {
-    if (!blogPostTitle.has(edge.node.title)) {
-      console.log(edge)
-      uniqueBlogPosts.push(edge)
-      blogPostTitle.add(edge.node.title)
-    }
-  })
-
   return (
     <div>
       <Layout>
@@ -41,7 +29,7 @@ const BlogPage = () => {
         <div>
           <Bounce bottom>
             <ol className={blogStyles.posts}>
-              {uniqueBlogPosts.map(edge => (
+              {data.allContentfulBlogPost.edges.map(edge => (
                 <li className={blogStyles.post}>
                   <Link to={`./${edge.node.slug}`}>
                     <h2>{edge.node.title}</h2>
